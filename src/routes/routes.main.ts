1fd58855e0159af78d7428b21d016aa8902c5ac1
AhmedEFRMElwazery <mohammed26418@alumni.itc.nl>
2022-10-28 13:33:09 +0200

    made few extra adjustments

--- src/routes/routes.main.ts
+++ src/routes/routes.main.ts
@@ -1,11 +1,11 @@
 import express, { Router, Request, Response } from 'express';
 import path from 'path';
 import logger from '../modules/logger.module';
-import images from './api/image';
+import imageEndpoint from './api/image.endpoint';
 
 const routes: Router = express.Router();
 
-routes.use('/v1/processimage', images);
+routes.use('/v1/processimage', imageEndpoint);
 
 routes.get('/', logger, (req: Request, res: Response): void => {
   res.sendFile(path.resolve('assets/pages/index.html'));
